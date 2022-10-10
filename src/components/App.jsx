511e743726b54f7702fc7f98345e2a51c008f4f5
AleksandrKatsinskiy <katsinskiy@gmail.com>
2022-10-10 19:29:01 +0200

    React 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
+import 'modern-normalize/modern-normalize.css';
+import Profile from './Profile/Profile';
+import user from '../data/user.json';
+import Statistics from './Statistics/Statistics';
+import data from '../data/data.json';
+import FriendsList from './Friends/Friends';
+import friends from '../data/friends.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
+export default function App() {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 20,
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        imageUrl={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="UPLOAD STATS" stats={data} />
+      <FriendsList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
-};
+}
